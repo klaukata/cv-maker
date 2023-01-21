7ff274e589b09a93159141c2a9c0c96d3206b196
berry <kathborowy@gmail.com>
2023-01-21 13:41:57 +0100

    cv comp

---
+++ src/components/CV.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class CV extends React.Component {
+    render() {
+        return (
+            <div className='CV'>
+                <div id='cv-personalDetails'>
+                    
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CV
