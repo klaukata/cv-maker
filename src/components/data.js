a66d02cbc1c18eddf383bcfc24f9b3c606765622
berry <kathborowy@gmail.com>
2023-01-23 05:47:38 +0100

    exaple data, first draft

---
+++ src/components/data.js
@@ -0,0 +1,10 @@
+let data = {
+    name: 'Jane Doe',
+    position: 'Software Developer',
+    number: '012 345 678',
+    mail: 'janedoe@kath.bo',
+    location: 'd00pa town',
+    description: 'lorem ipsum'
+};
+
+export default data;
