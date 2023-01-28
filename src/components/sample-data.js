81df83f347375bee51b7676c74eb39ef14263148
berry <kathborowy@gmail.com>
2023-01-28 09:31:02 +0100

    changed key name

--- src/components/sample-data.js
+++ src/components/sample-data.js
@@ -1,7 +1,7 @@
 let data = {
     cvName: 'Jane Doe',
     cvPosition: 'Software Developer',
-    cvNumber: '012 345 678',
+    cvPhone: '012 345 678',
     cvMail: 'janedoe@kath.bo',
     cvLocation: 'd00pa town',
     cvDescription: 'lorem ipsum'
