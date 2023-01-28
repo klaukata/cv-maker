b99edbc7f391d25162e618a0b24b3652a7265643
berry <kathborowy@gmail.com>
2023-01-28 09:24:17 +0100

    displays personal details

--- src/components/CV.js
+++ src/components/CV.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import data from './sample-data';
+import uniqid from "uniqid";
 
 class CV extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = data;
+    }
+    createAndFillElements() {
+        let keysArr = Object.keys(this.state);
+        let els = keysArr.map((key) => <p id={key} key={uniqid()}>{this.state[key]}</p>);
+        return (els)
+    }
     render() {
         return (
             <div className='CV'>
-                <div id='cv-personalDetails'>
-                    
-                </div>
+                {this.createAndFillElements()}
             </div>
         )
     }
