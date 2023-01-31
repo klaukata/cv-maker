cb274662b4f7f8596ef5e25fc97386c6de708d39
berry <kathborowy@gmail.com>
2023-01-31 05:58:38 +0100

    added work exp section

--- src/components/sample-data.js
+++ src/components/sample-data.js
@@ -1,10 +1,42 @@
 let data = {
-    cvName: 'Jane Doe',
-    cvPosition: 'Software Developer',
-    cvPhone: '012 345 678',
-    cvMail: 'janedoe@kath.bo',
-    cvLocation: 'd00pa town',
-    cvDescription: 'lorem ipsum'
+    personalData: {
+        cvName: 'Jane Doe',
+        cvPosition: 'Software Developer',
+        cvPhone: '012 345 678',
+        cvMail: 'janedoe@kath.bo',
+        cvLocation: 'd00pa town',
+        cvDescription: 'lorem ipsum'
+    },
+    workExperience: {
+        work1: {
+            companyName: 'Company A',
+            position: 'Intern',
+            date: {
+                start: '2015',
+                end: '2016'
+            },
+            description: 'Lorem ipsum'
+        },
+        work2: {
+            companyName: 'Company B',
+            position: 'Intern',
+            date: {
+                start: '2015',
+                end: '2016'
+            },
+            description: 'Lorem ipsum'
+        },
+        work3: {
+            companyName: 'Company C',
+            position: 'Intern',
+            date: {
+                start: '2015',
+                end: '2016'
+            },
+            description: 'Lorem ipsum'
+        }
+    }
+    
 };
 
 export default data;
