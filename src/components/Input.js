c645ab0e28844479cd5615f227e8999fa42f3cff
berry <kathborowy@gmail.com>
2023-01-31 05:58:10 +0100

    input has specified value

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import data from './sample-data';
 
 class Input extends React.Component {
     constructor() {
         super();
         this.insertTxt = this.insertTxt.bind(this)
-      }
+    }
+    componentDidMount() {
+        console.log(this.props.placeholder);
+    }
     insertTxt(el) {
         let id = 'cv' + el.target.getAttribute('id').slice(5);
         let text = el.target.value;
         document.getElementById(id).textContent = text;
     }
     render() {
+        let placeholder = this.props.placeholder;
         return (
-            <input type="text" id={'form-' + this.props.placeholder} placeholder={this.props.placeholder} onInput={(el) => this.insertTxt(el)}/>
+            <input type="text" id={'form-' + placeholder} 
+                placeholder={placeholder} onInput={(el) => this.insertTxt(el)}
+                value={data['personalData']['cv' + placeholder]}
+            />
         )
     }
 };
