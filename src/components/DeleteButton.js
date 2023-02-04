41be4672d2cbd5f775a7a2c1777fc6ad25a738cf
berry <kathborowy@gmail.com>
2023-02-04 11:12:48 +0100

    rm button works

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 class DeleteButton extends React.Component {
-    eventfunc() {
-        console.log('hi');
+    eventfunc(e) {
+        let el = e.target.parentElement;
+        let className = el.classList[0];
+        let idName = 'cv-' + className;
+        let cvEl = document.getElementById(idName)
+        console.log(cvEl);
+        el.remove();
+        cvEl.remove()
     }
     render() {
         return (
-            <button type='button' onClick={this.eventfunc}>DELETE</button>
+            <button type='button' onClick={(e) => this.eventfunc(e)}>DELETE</button>
         )
     }
 }
