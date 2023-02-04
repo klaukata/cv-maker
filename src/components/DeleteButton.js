1f507bf4995e8d480c8ad47202342af900743ce6
berry <kathborowy@gmail.com>
2023-02-04 09:17:47 +0100

    page doesnt reloads when clicked

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 class DeleteButton extends React.Component {
+    eventfunc() {
+        console.log('hi');
+    }
     render() {
         return (
-            <button>DELETE</button>
+            <button type='button' onClick={this.eventfunc}>DELETE</button>
         )
     }
 }
