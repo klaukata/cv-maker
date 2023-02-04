ccc225b8434526766d9e14d14787f6cb0ef1802f
berry <kathborowy@gmail.com>
2023-02-04 11:02:51 +0100

    rm sample-data import

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
 import React from 'react';
 import './App.css';
 import Form from './components/Form'
-import CV from './components/CV'
+import CV from './components/CV';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    // this.state = {
-    //   personalDetails: {
-    //     name: '',
-    //     position: '',
-    //     phoneNumber: '',
-    //     mail: '',
-    //     location: '',
-    //     description: ''
-    //   },
-    //   experience: [],
-    //   education: []
-    // }
-    // this.insertTxt = this.insertTxt.bind(this)
-  }
-  // insertTxt(el) {
-  //   let text = el.value;
-  //   document.getElementById('ass').textContent = text; 
-  // }
   render() {
     return (
       <>
