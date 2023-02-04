b51b6421221ad3ca0bdc11dfef356c356691ab2d
berry <kathborowy@gmail.com>
2023-02-04 17:04:03 +0100

    moved files

---
+++ src/components/sample-data/Input.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import data from './sample-data';
+
+class Input extends React.Component {
+    constructor() {
+        super();
+        this.insertTxt = this.insertTxt.bind(this);
+    }
+    componentDidMount() {
+        let placeholder = this.props.placeholder;
+        if (Object.keys(data['personalData']).includes(placeholder)) {
+            document.getElementById('form-' + this.props.placeholder).value = data['personalData'][this.props.placeholder];
+        } else if (placeholder.startsWith('work')) {
+            let work = placeholder.slice(0, 5);
+            let key = placeholder.slice(5);
+            document.getElementById('form-' + this.props.placeholder).value = data['workExperience'][work][key];
+        } else if (placeholder.startsWith('edu')) {
+            let edu = placeholder.slice(0, 5);
+            let key = placeholder.slice(5);
+            document.getElementById('form-' + this.props.placeholder).value = data['education'][edu][key];
+        }
+    }
+    insertTxt(el) {
+        let id = 'cv-' + el.target.getAttribute('id').slice(5);
+        let text = el.target.value;
+        document.getElementById(id).textContent = text;
+    }
+    slicePlaceholder(ph) {
+        if (ph.startsWith('work') || ph.startsWith('educ')) {
+            ph = ph.slice(5)
+        } 
+        return ph
+    }
+    render() {
+        let placeholder = this.props.placeholder;
+        return (
+            <input type="text" id={'form-' + placeholder} 
+                placeholder={this.slicePlaceholder(placeholder)} onInput={(el) => this.insertTxt(el)}
+            />
+        )
+    }
+};
+
+export default Input;
---
+++ src/components/sample-data/sample-data.js
@@ -0,0 +1,46 @@
+let data = {
+    personalData: {
+        Name: 'Jane Doe',
+        Position: 'Software Developer',
+        Phone: '012 345 678',
+        Mail: 'janedoe@kath.bo',
+        Location: 'd00pa town',
+        Description: 'lorem ipsum'
+    },
+    workExperience: {
+        work1: {
+            Company: 'Company A',
+            Position: 'Intern',
+            Start: '2015',
+            End: '2016',
+            Description: 'Lorem ipsum'
+        },
+        work2: {
+            Company: 'Company B',
+            Position: 'Intern',
+            Start: '2015',
+            End: '2016',
+            Description: 'Lorem ipsum'
+        },
+        work3: {
+            Company: 'Company C',
+            Position: 'Intern',
+            Start: '2015',
+            End: '2016',
+            Description: 'Lorem ipsum'
+        }
+    },
+    education: {
+        educ1: {
+            Course: 'Bidibi MDNA',
+            University: 'Oompa Loompa School',
+            Start: '2015',
+            End: '2022',
+            Description: 'Lorem ipsum'    
+        }
+        
+    }
+    
+};
+
+export default data;
