125f0bde6e22cc1d4a523b369e9f1bbded0a03ae
berry <kathborowy@gmail.com>
2023-02-04 09:12:30 +0100

    returns a button

---
+++ src/components/DeleteButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class DeleteButton extends React.Component {
+    render() {
+        return (
+            <button>DELETE</button>
+        )
+    }
+}
+export default DeleteButton;
