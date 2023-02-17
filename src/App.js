8b18fca943ed050146fcb2c2769a3fac4c295fdd
berry <kathborowy@gmail.com>
2023-02-17 17:27:34 +0100

    prettier

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import './App.css';
 import Form from './components/Form'
 import CV from './components/CV';
 
+import CVSection from './components/CVAddedSection';
+
 class App extends React.Component {
+  state = {
+    numOfAddedExpDivs: 0
+  }
+
+  onAddExpDiv = () => {
+        this.setState({
+          numOfAddedExpDivs: this.state.numOfAddedExpDivs + 1
+        });
+  }
+
+
   render() {
+    const children = [];
+    
+    for (var i = 0; i < this.state.numOfAddedExpDivs; i += 1) {
+      children.push(<CVSection />);
+    };
+    
     return (
       <>
-        <div className='App-form'>
-          <Form />
-        </div>
-        <CV />
+        <Form onAddChild={this.onAddExpDiv}>
+        </Form>
+        <CV onAddChild={this.onAddExpDiv}>
+          {children}
+        </CV>
       </>
     ); 
   }
