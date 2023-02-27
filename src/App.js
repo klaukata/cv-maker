cde8173baccce2801e6e1df9edf0f751c76c7831
berry <kathborowy@gmail.com>
2023-02-27 14:48:27 +0100

    increments state

--- src/App.js
+++ src/App.js
@@ -2,34 +2,40 @@ import React from 'react';
 import './App.css';
 import Form from './components/Form'
 import CV from './components/CV';
-
 import CVSection from './components/CVAddedSection';
 
 class App extends React.Component {
   state = {
-    numOfAddedExpDivs: 0
+    numOfAddedExpDivs: 0,
+    numOfAddedEduDivs: 0
   }
 
-  onAddExpDiv = () => {
-        this.setState({
-          numOfAddedExpDivs: this.state.numOfAddedExpDivs + 1
-        });
+  onAddExpDiv = (section) => {
+    if (section === 'workExperience') {
+      this.setState({
+        numOfAddedExpDivs: this.state.numOfAddedExpDivs + 1
+      });
+    } else if (section === 'education') {
+      this.setState({
+        numOfAddedEduDivs: this.state.numOfAddedEduDivs + 1
+      });
+    }
+        
   }
 
 
   render() {
-    const children = [];
+    const childrenExp = [];
     
-    for (var i = 0; i < this.state.numOfAddedExpDivs; i += 1) {
-      children.push(<CVSection />);
+    for (var i = 1; i < this.state.numOfAddedExpDivs + 1; i += 1) {
+      childrenExp.push(<CVSection num={i}/>);
     };
     
     return (
       <>
-        <Form onAddChild={this.onAddExpDiv}>
-        </Form>
+        <Form onAddChild={this.onAddExpDiv} num={this.state.numOfAddedExpDivs} />
         <CV onAddChild={this.onAddExpDiv}>
-          {children}
+          {childrenExp}
         </CV>
       </>
     ); 
