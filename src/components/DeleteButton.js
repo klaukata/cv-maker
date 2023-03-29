d390adc13f39d75b314b373e128fedee7af95af5
berry <kathborowy@gmail.com>
2023-03-29 15:22:32 +0200

    rm console.log

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -3,7 +3,7 @@ import React from 'react';
 class DeleteButton extends React.Component {
     eventfunc(e) {
         let el = e.target.parentElement;
-        let idName = ('cv-' + el.id).replace(/-workExperience/ig, '');
+        let idName = ('cv-' + el.id)
         let cvEl = document.getElementById(idName);
         el.style.cssText = 'display: none !important';
         cvEl.style.display = 'none';
