ff69885ce428341224fa85f57f1776a15d3b3359
berry <kathborowy@gmail.com>
2023-04-08 14:53:25 +0200

    works 4 a button thats wrapped in a div

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 class DeleteButton extends React.Component {
     eventfunc(e) {
-        let el = e.target.parentElement;
-        let idName = ('cv-' + el.id)
+        let el = e.target.parentElement.parentElement;
+        let idName = ('cv-' + el.id);
         let cvEl = document.getElementById(idName);
         el.style.cssText = 'display: none !important';
         cvEl.style.display = 'none';
     }
     render() {
         return (
-            <button type='button' onClick={(e) => this.eventfunc(e)}>DELETE</button>
+            <div className='deleteButton'>
+                <button type='button' onClick={(e) => this.eventfunc(e)}>DELETE</button>
+            </div>
+            
         )
     }
 }
