6701c6a1a2df8515b2f0fe649d6afd6ac44718e0
berry <kathborowy@gmail.com>
2023-04-09 18:01:02 +0200

    realistic data change

--- src/components/sample-data/sample-data.js
+++ src/components/sample-data/sample-data.js
@@ -4,39 +4,32 @@ let data = {
         Position: 'Software Developer',
         Phone: '012 345 678',
         Mail: 'janedoe@kath.bo',
-        Location: 'd00pa town',
-        Description: 'lorem ipsum'
+        Location: 'Rzeszow, Poland',
+        Description: 'Ut turpis lorem, tincidunt eu nibh a, iaculis feugiat eros. Proin interdum odio magna, eget ultricies dolor viverra sodales. Maecenas ullamcorper imperdiet libero eget ultrices. Donec vel augue eu magna porttitor aliquet laoreet ac odio. Curabitur et viverra quam. Integer ac mi ac mauris iaculis mattis.'
     },
     workExperience: {
         work1: {
-            Company: 'Company A',
             Position: 'Intern',
-            Start: '2015',
-            End: '2016',
-            Description: 'Lorem ipsum'
+            Company: 'IT Company',
+            Start: '2019',
+            End: '2020',
+            Description: 'Aliquam consequat nunc sit amet orci faucibus ullamcorper. Cras consectetur turpis tellus, vel egestas massa aliquam quis. Curabitur nec malesuada arcu.'
         },
         work2: {
-            Company: 'Company B',
-            Position: 'Intern',
-            Start: '2015',
-            End: '2016',
-            Description: 'Lorem ipsum'
-        },
-        work3: {
-            Company: 'Company C',
-            Position: 'Intern',
-            Start: '2015',
-            End: '2016',
-            Description: 'Lorem ipsum'
+            Position: 'Software Developer',
+            Company: 'Another Company',
+            Start: '2020',
+            End: 'Present',
+            Description: 'Nunc a odio ut diam viverra ultrices in at libero. Donec pellentesque nisi a tellus dictum, a porta leo facilisis. Nam consectetur neque non erat fringilla, vel tristique sapien ultrices.'
         }
     },
     education: {
         educ1: {
-            Course: 'Bidibi MDNA',
-            University: 'Oompa Loompa School',
-            Start: '2015',
-            End: '2022',
-            Description: 'Lorem ipsum'    
+            Course: 'Computer Science',
+            University: 'University of Edinburgh',
+            Start: '2018',
+            End: '2021',
+            Description: 'Aliquam tortor quam, laoreet feugiat lectus sed, euismod bibendum enim. Maecenas laoreet aliquet mauris id accumsan. Etiam commodo sit amet orci quis feugiat.'    
         }
         
     }
